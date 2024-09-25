00a783017872f1827700b51fd92582b974a65b82
desolee7 <125908645+desolee7@users.noreply.github.com>
2024-09-26 02:08:02 +0300

    под хромову с кайфом делается кста попробуй

---
+++ main.py
@@ -0,0 +1,93 @@
+import requests
+import streamlit as st
+import base64
+
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"] {  
+    background-image: url("https://sun9-13.userapi.com/impg/Yp2wYB2uWR8oMi3bjLUyoQMyenIq7G2pDA9Qsw/LAYhT_K3z9A.jpg?size=2560x1402&quality=96&sign=4d19a05796bc4b2b7ac2035839d4926a&type=album");     
+    background-size: cover;
+}
+[data-testid="stHeader"]{                    
+    background-color: rgba(0, 0, 0, 0);
+}
+[data-testid="stSidebar"]{                    
+    background-color: black;
+}
+
+.st-ax {
+    background-color: rgb(188,22,226);
+}
+
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+gradient_text_html = """
+<style>
+.gradient-text {
+    background-color: rgb(253,255,254);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 46px;
+    font-family: Optima, sans-serif;
+    font-weight: bold;
+    padding: 10px;
+    text-align: center;
+}
+
+
+</style>
+
+<div class="gradient-text">Тэги для видео</div>
+"""
+
+
+st.markdown(gradient_text_html, unsafe_allow_html=True)
+
+def extract_video(uploaded_video):
+    video_content = uploaded_video.read()
+    encoded_video_content = base64.b64encode(video_content).decode('utf-8')
+    return encoded_video_content
+
+video_input_option = st.radio('Выберите способ добавления видео', ('Загрузить видеофайл', 'Вставить ссылку на RuTube'))
+
+video_title = st.text_input('Введите название видео')
+video_description = st.text_area('Введите описание видео', height=100)
+
+if video_input_option == 'Загрузить видеофайл':
+    uploaded_video = st.file_uploader('Перетащите видео файл', type=["mp4", "avi", "mov"])
+
+    if uploaded_video is not None and st.button("Загрузить видео"):
+        encoded_video_content = extract_video(uploaded_video)
+        
+        data_to_send = {
+            "video_title": video_title,
+            "video_description": video_description,
+            "video_content": encoded_video_content
+        }
+        
+        response = requests.post('https://echo.free.beeceptor.com', json=data_to_send)
+        
+        st.video(uploaded_video)
+        st.write(f"Название видео: {video_title}")
+        st.write(f"Описание видео: {video_description}")
+
+elif video_input_option == 'Вставить ссылку на RuTube':
+    video_url = st.text_input('Вставьте ссылку на видео с RuTube')
+
+    if video_url and st.button("Загрузить по ссылке"):
+        video_embed_code = f"""
+        <iframe src="{video_url}" width="700" height="400" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        """
+        data_to_send = {
+            "video_title": video_title,
+            "video_description": video_description,
+            "video_url": video_url
+        }
+        
+        response = requests.post('https://echo.free.beeceptor.com', json=data_to_send)
+        
+        st.markdown(video_embed_code, unsafe_allow_html=True)  
+        st.write(f"Название видео: {video_title}")
+        st.write(f"Описание видео: {video_description}")
